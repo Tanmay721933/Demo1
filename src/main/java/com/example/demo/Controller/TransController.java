9ce54f34d930f2b43665d549e7686be83bd953d0
Tanmayy7879 <deshpandetanmay01@gmail.com>
2024-07-24 15:37:42 +0530

    message histry check

--- src/main/java/com/example/demo/Controller/TransController.java
+++ src/main/java/com/example/demo/Controller/TransController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransController {
     @GetMapping
     public void getData(){
-        System.out.println("print data which is not useful");
+        System.out.println("print data which ");
+        System.out.println("addded this new line of code in second branch");
+        System.out.println("added some lines of code to check commit history");
     }
-
 }
+
